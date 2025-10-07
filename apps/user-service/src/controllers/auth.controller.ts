f67b11e487e51b885d465851d75ca96a6070ebee
AnjaliSoni018 <anjali.soni@thoughtwin.com>
2025-10-07 12:35:51 +0530

    solve docker error

--- apps/user-service/src/controllers/auth.controller.ts
+++ apps/user-service/src/controllers/auth.controller.ts
@@ -1,34 +1,3 @@
-// import { Request, Response, NextFunction } from 'express';
-// import * as authService from '../services/auth.service';
-
-// export const sendOtpController = async (
-//   req: Request,
-//   res: Response,
-//   next: NextFunction
-// ) => {
-//   try {
-//     const { phone } = req.body;
-//     const result = await authService.sendOtp(phone);
-//     res.status(200).json(result);
-//   } catch (err) {
-//     next(err);
-//   }
-// };
-
-// export const verifyOtpController = async (
-//   req: Request,
-//   res: Response,
-//   next: NextFunction
-// ) => {
-//   try {
-//     const { phone, otp, name, email } = req.body;
-//     const result = await authService.verifyOtp(phone, otp, name, email);
-//     res.status(200).json(result);
-//   } catch (err) {
-//     next(err);
-//   }
-// };
-// apps / user - service / src / controllers / auth.controller.ts;
 import { Request, Response, NextFunction } from 'express';
 import * as authService from '../services/auth.service';
 
