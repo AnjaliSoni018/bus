6bd8e53e322e630db30aed34c07cc29aa771bbdf
AnjaliSoni018 <anjali.soni@thoughtwin.com>
2025-10-08 11:37:16 +0530

    solve cors error

--- apps/user-service/src/main.ts
+++ apps/user-service/src/main.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import userRoutes from './routes/user.routes';
 import authRoutes from './routes/auth.routes';
 import prisma from './config/prisma.service';
@@ -9,6 +10,7 @@ import { errorMessages } from './constants/errorMessages';
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
